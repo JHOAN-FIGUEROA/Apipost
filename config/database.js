c4aff01328b5a013ad291beea58da77a679c7108
lafigura <jhoan24figueroa@gmail.com>
2025-05-07 18:51:10 -0500

    nueva base de datos

--- config/database.js
+++ config/database.js
@@ -1,17 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  port: process.env.DB_PORT,
+  protocol: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
+      rejectUnauthorized: false // necesario para Render
     }
   },
-  logging: false,
+  logging: false, // opcional: desactiva logs de SQL en consola
 });
 
 module.exports = sequelize;
